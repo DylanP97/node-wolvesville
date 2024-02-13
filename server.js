b2c25a34b35287aa84f3f50f4a5923536c9b91c7
dylanP97 <dylan.pinheiro.1997@gmail.com>
2024-02-13 15:11:31 +0100

    implementing registeredActions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./index");
 const socketIO = require("socket.io");
 const { initializePlayersList } = require("./gameEvents");
-const { checkForWinner } = require("./lib/gameActions");
+const { checkForWinner, murder } = require("./lib/gameActions");
 
 const normalizePort = (val) => {
   const port = parseInt(val, 10);
@@ -94,7 +94,7 @@ io.on("connection", (socket) => {
         roomToJoin = {
           ...roomToJoin,
           playersList: playersList,
-          aliveList: null,
+          newAliveList: playersList.filter((p) => p.isAlive),
           dayCount: 0,
           timeOfTheDay: "nighttime",
           timeCounter: 20000,
@@ -118,12 +118,31 @@ io.on("connection", (socket) => {
             gameToUpdate.timeCounter -= 1000;
             if (gameToUpdate.timeCounter == 0) {
               if (gameToUpdate.timeOfTheDay == "nighttime") {
+                let newPlayersList
+                gameToUpdate.registeredActions.forEach((action) => {
+                  if (action.type === "murder") {
+                    newPlayersList = gameToUpdate.playersList.map((ply) => {
+                      if (ply.id === action.selectedPlayerId) {
+                        return {
+                          ...ply,
+                          isAlive: false,
+                        }
+                      } else {
+                        return ply;
+                      }
+                    })
+                  }
+                });
+                if (newPlayersList) {
+                  gameToUpdate.playersList = newPlayersList;
+                  gameToUpdate.newAliveList = newPlayersList.filter((p) => p.isAlive)
+                }
                 gameToUpdate.timeOfTheDay = "daytime"
                 gameToUpdate.timeCounter = 10000
                 gameToUpdate.dayCount += 1
                 gameToUpdate.messagesHistory.push({ author: "Game", msg: "It's a new day here in the village." })
               } else if (gameToUpdate.timeOfTheDay == "daytime") {
-                gameToUpdate.timeCounter = 30000
+                gameToUpdate.timeCounter = 15000
                 gameToUpdate.timeOfTheDay = "votetime"
                 gameToUpdate.messagesHistory.push({ author: "Game", msg: "It's time to vote." })
               } else if (gameToUpdate.timeOfTheDay == "votetime") {
@@ -231,6 +250,17 @@ io.on("connection", (socket) => {
     games.push(gameToUpdate);
     io.to(roomId).emit("updateGame", gameToUpdate);
   });
+
+  socket.on("registerAction", (actionObject) => {
+    let gameToUpdate = games.find((room) => room.id === roomId);
+
+    gameToUpdate.registeredActions.push(actionObject);
+
+    const newGames = games.filter((r) => r.id != roomId)
+    games = newGames;
+    games.push(gameToUpdate);
+    io.to(roomId).emit("updateGame", gameToUpdate);
+  })
 
   socket.on("disconnect", () => {
     let user = connectedUsers.indexOf(connectedUsers.find(user => user.socketId == socket.id))
